0015f59fae9922cf4aaf21d977a66f4735a6ffdd
zalesky <zalesky@ukr.net>
2016-07-12 00:33:48 +0300

    tests

---
+++ week05/calc.js
@@ -0,0 +1,4 @@
+function calc (argument) {
+  var result = 0;
+  return result;
+}
---
+++ week05/test/test.js
@@ -0,0 +1,14 @@
+"use strict";
+var assert = require("assert");
+var calc = require("../calc");
+
+describe("Calc-test", function () {
+
+  specify("should return 0 for an empty string", function() {
+    assert.equal(calc(''), 0);
+  });
+
+
+
+  
+});
