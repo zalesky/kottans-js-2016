e4f65f8cbfc8e5087a27dbd12023953624db9d65
zalesky <zalesky@ukr.net>
2016-07-12 15:21:00 +0300

    KATA#1 done.

--- week05/calc.js
+++ week05/calc.js
@@ -1,4 +1,16 @@
-function calc (argument) {
+calc={};
+calc.add = function(argument) {
+  var array = argument.match(/(-?[0-9])+/g) || [];
   var result = 0;
+  var length = array.length;
+  for (;length--;) {
+    if(array[length] < 0)
+      throw new Error('negatives not allowed ' + array[length]);
+    if (array[length] > 1000) continue;
+    result += +array[length];
+  }
+
   return result;
-}
+};
+
+module.exports = calc.add;
--- week05/test/test.js
+++ week05/test/test.js
@@ -1,14 +1,37 @@
 "use strict";
 var assert = require("assert");
-var calc = require("../calc");
+var add = require("../calc");
 
 describe("Calc-test", function () {
 
   specify("should return 0 for an empty string", function() {
-    assert.equal(calc(''), 0);
+    assert.equal(add(''), 0);
+  });
+  specify("should return the same value for 1 argument", function() {
+      assert.equal(add('3'), 3);
+    });
+  specify("should adds 2 inputs separated by coma", function() {
+    assert.equal(add('1,2'), 3);
+  });
+  specify('should handle an unknown amount of numbers', function() {
+    assert.equal(add('1,2,3,4,5'), 15);
+    assert.equal(add('1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5'), 60);
   });
 
+  it('should handle new lines between numbers', function() {
+    assert.equal(add('1,\n2,3,\n4,5'), 15);
+  });
 
+  it('should Support different delimiters', function() {
+    assert.equal(add('1,\n2,as3,\n4sdd,f5'), 15);
+    assert.equal(add('1:,\n2,as3,\n4sdd,f5:'), 15);
+  });
 
-  
+  it('negative numbers should return exeption', function() {
+    assert.throws(function(){add('-1')}, Error, "negatives not allowed -1");
+  });
+
+  it('should ignore numbers bigger than 1000', function() {
+    assert.equal(add('1,1001,23000'), 1);
+  })
 });
